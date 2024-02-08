a2c86ee14286b4e5b6ad660abaad864e4b1e9a62
Matthew Oshimo <moshimo90696@gmail.com>
2024-02-07 16:54:23 -0800

    all tests passed

--- src/lib.js
+++ src/lib.js
@@ -1,15 +1,59 @@
 import fs from 'fs'
 
-export const chooseRandom = () => {
-  // TODO implement chooseRandom
+export const chooseRandom = (array1, numItems = 0) => {
+
+  if(array1.length <= 1 ){
+    return array1;
+  }
+
+  if(numItems > array1.length || numItems < 1){
+    numItems = 1 + Math.floor(Math.random() * array1.length);
+  }
+
+  let finalArray = [];
+
+  for(let i = 0; i < numItems; i++){
+    finalArray.push(array1[Math.floor(Math.random() * array1.length)]);
+  }
+  return finalArray;
 }
 
-export const createPrompt = () => {
-  // TODO implement createPrompt
+export const createPrompt = (items = {numQuestions: 1, numChoices: 2}) => {
+  let finalArray = [];
+  if(items == null){
+    return finalArray;
+  }
+  items.numQuestions = items.numQuestions > 1 ? items.numQuestions: 1;
+  items.numChoices = items.numChoices > 2 ? items.numChoices : 2;
+  for( let i = 1; i <= items.numQuestions; i++){
+    finalArray.push({type: "input", name: `question-${i}`, message: `Enter question ${i}`});
+    for(let j = 1; j <= items.numChoices; j++){
+      finalArray.push({type: "input", name: `question-${i}-choice-${j}`, message: `Enter answer choice ${j} for question ${i}`})
+    };
+  }
+  return finalArray;
+
 }
 
-export const createQuestions = () => {
-  // TODO implement createQuestions
+export const createQuestions = (item = {}) => {
+  let finalArray = [];
+    for(let i in item){
+      if(!i.includes('choice'))
+      {
+        finalArray.push({type: 'list', name: `${i}`, message: `${item[i]}`, choices: []});
+      }
+    }
+
+    for(let i in item){
+      for(let f in finalArray){
+        let quest = finalArray[f];
+        if(i.includes(quest.name) && i.includes('choice')){
+           finalArray[f].choices.push(item[i]);
+        }
+      }
+    }
+
+  return finalArray;
 }
 
 export const readFile = path =>
